a3230253ca71c86012a3f44ae4e697f882e05789
gideonlangat98 <gideonlangat98@gmail.com>
2022-07-20 12:02:06 +0300

    Done.

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  if(!props.isPublished){
+    return null;
+  }else{
+    return <div>
+      <h1>{props.articleText}</h1>
+      <p>{props.minutesToRead} minutesToRead</p>
+      </div>;
+  }
 }
 
 export default BlogContent;
